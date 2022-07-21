6ae8233539ef68faee2d37c56bd0bc5863fcc5cc
Stiven Pinilla <brayan.pinilla@servinformacion.com>
2022-07-21 14:03:50 -0500

    llamar funciones con python

--- sesion_cinco/funciones1.py
+++ sesion_cinco/funciones1.py
@@ -2,8 +2,15 @@
 
 import funciones'''#formas de llamar funciones desde otras fuentes.
 
-
+#llamar funciones "completas"
 
 import funciones
 
 funciones.HolaMundo() 
+
+
+
+#llamar funciones especificas
+from funciones import HolaMundo, sumNumeros, esPar
+
+
