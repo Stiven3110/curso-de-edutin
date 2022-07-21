d3f93d52901468084343fea6b0c053baf5dfb92e
Stiven Pinilla <brayan.pinilla@servinformacion.com>
2022-07-21 11:20:06 -0500

    agregando-lambdas

---
+++ sesion_cinco/lambdas.py
@@ -0,0 +1,14 @@
+# lambdas
+
+x = lambda a : a + 10
+print(x(20))
+
+y = lambda a, b : a * b
+print(y(2, 5))
+
+z = lambda a, b, c : a * b / c
+print(z(2, 5, 8))
+
+j = lambda a, b, c : a * b + c / 10
+print(j(4,6,8))
+
